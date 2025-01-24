593000ca890a8d960065cd98df6affdd31038080
nexgensoft0 <nexgensoft0@gmail.com>
2025-01-24 08:02:07 +0600

    for coffe user crud oparetion done

--- index.js
+++ index.js
@@ -36,9 +36,40 @@ async function run() {
       const result = await user.toArray();
       res.send(result);
     });
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeUsersCollection.findOne(query);
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await coffeeUsersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const userData = {
+        $set: {
+          name: user.name,
+        },
+      };
+      const result = await coffeeUsersCollection.updateOne(
+        query,
+        userData,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeUsersCollection.deleteOne(query);
       res.send(result);
     });
 
